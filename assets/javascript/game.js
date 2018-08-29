0499000b533f23f00416c9ffd13e87aa1e64fbc9
akatzer <akatzer@live.com>
2018-08-29 18:07:36 -0500

    8.29 push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
+    var tonyNum = 0;
     var wins = 0;
     var losses = 0;
     
+    $("#tony-num").text(tonyNum);
 
+    thanosNum = Math.floor((Math.random() * 102) + 19);
+    $("#thanos-num").text(thanosNum);
 
     // Setting your values for everything
-    function startGame() {
-        var tonyNum = 0
+    function resetGame() {
+        tonyNum = 0
         $("#tony-num").text(tonyNum);
 
         thanosNum = Math.floor((Math.random() * 102) + 19);
@@ -20,27 +24,33 @@ $(document).ready(function () {
         timeNum = Math.floor((Math.random() * 12) + 1);
 
         $(".mind").on("click", function () {
-            tonyNum = tonyNum + mindNum;
+            tonyNum += mindNum;
+            $("#tony-num").text(tonyNum);
             console.log(tonyNum);
         })
         $(".power").on("click", function () {
             tonyNum += powerNum;
+            $("#tony-num").text(tonyNum);
             console.log(tonyNum);
         })
         $(".reality").on("click", function () {
             tonyNum += realityNum;
+            $("#tony-num").text(tonyNum);
             console.log(tonyNum);
         })
         $(".soul").on("click", function () {
             tonyNum += soulNum;
+            $("#tony-num").text(tonyNum);
             console.log(tonyNum);
         })
         $(".space").on("click", function () {
             tonyNum += spaceNum;
+            $("#tony-num").text(tonyNum);
             console.log(tonyNum);
         })
         $(".time").on("click", function () {
             tonyNum += timeNum;
+            $("#tony-num").text(tonyNum);
             console.log(tonyNum);
             console.log(thanosNum);
         })
