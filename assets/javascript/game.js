91f2017fb932783b8262c67cb5fbd6730a37f734
akatzer <akatzer@live.com>
2018-08-30 20:11:30 -0500

    8.30 commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,80 @@
 $(document).ready(function () {
     var wins = 0;
     var losses = 0;
-    
+    var tonyNum = 0;
+    var thanosNum = 0;
 
 
     // Setting your values for everything
-    function startGame() {
-        var tonyNum = 0
-        $("#tony-num").text(tonyNum);
 
+    function resetGame() {
+        tonyNum = 0
+        $("#tony-num").text(tonyNum);
         thanosNum = Math.floor((Math.random() * 102) + 19);
         $("#thanos-num").text(thanosNum);
-
         mindNum = Math.floor((Math.random() * 12) + 1);
         powerNum = Math.floor((Math.random() * 12) + 1);
         realityNum = Math.floor((Math.random() * 12) + 1);
         soulNum = Math.floor((Math.random() * 12) + 1);
         spaceNum = Math.floor((Math.random() * 12) + 1);
         timeNum = Math.floor((Math.random() * 12) + 1);
+    }
 
-        $(".mind").on("click", function () {
-            tonyNum = tonyNum + mindNum;
-            console.log(tonyNum);
-        })
-        $(".power").on("click", function () {
-            tonyNum += powerNum;
-            console.log(tonyNum);
-        })
-        $(".reality").on("click", function () {
-            tonyNum += realityNum;
-            console.log(tonyNum);
-        })
-        $(".soul").on("click", function () {
-            tonyNum += soulNum;
-            console.log(tonyNum);
-        })
-        $(".space").on("click", function () {
-            tonyNum += spaceNum;
-            console.log(tonyNum);
-        })
-        $(".time").on("click", function () {
-            tonyNum += timeNum;
-            console.log(tonyNum);
-            console.log(thanosNum);
-        })
 
-        
-        }   
+    $(".mind").on("click", function () {
+        tonyNum += mindNum;
+        $("#tony-num").text(tonyNum);
+        logic();
+    })
+    $(".power").on("click", function () {
+        tonyNum += powerNum;
+        $("#tony-num").text(tonyNum);
+        logic();
+    })
+    $(".reality").on("click", function () {
+        tonyNum += realityNum;
+        $("#tony-num").text(tonyNum);
+        logic();
+    })
+    $(".soul").on("click", function () {
+        tonyNum += soulNum;
+        $("#tony-num").text(tonyNum);
+        logic();
+    })
+    $(".space").on("click", function () {
+        tonyNum += spaceNum;
+        $("#tony-num").text(tonyNum);
+        logic();
+    })
+    $(".time").on("click", function () {
+        tonyNum += timeNum;
+        $("#tony-num").text(tonyNum);
+        logic();
+    })
+
+    function logic() {
         if (tonyNum === thanosNum) {
             alert("YOU WIN!");
             wins++;
-            startgame();
+            $(".losses").text(wins)
+            resetGame();
         }
         else if (tonyNum > thanosNum) {
             alert("YOU LOSE!");
             losses++;
-            startgame();
+            $(".losses").text(losses);
+            resetGame();
+        }
     }
 
 
 
 
 
-    
 
 
 
 
-
-
-
-
-    startGame()
+    resetGame();
 
 });
